502f42967dcc6c27b8f3971fba8f2250c62717d5
petrkalashenko3 <petrkalashenko3@gmail.com>
2026-04-29 01:12:11 +0700

    updated UnitMover with logic of selection

--- Assets/UnitMover.cs
+++ Assets/UnitMover.cs
@@ -4,15 +4,38 @@ using UnityEngine.AI;
 public class UnitMover : MonoBehaviour
 {
     private NavMeshAgent agent;
+    private bool isSelected = false;
+    private Renderer unitRenderer;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        unitRenderer = GetComponent<Renderer>();
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform == this.transform)
+                {
+                    isSelected = true;
+                    unitRenderer.material.color = Color.green;
+                }
+                else
+                {
+                    isSelected = false;
+                    unitRenderer.material.color = Color.white;
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1) && isSelected) 
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
